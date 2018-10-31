016978df53ae7d17502cfa39d9e744013a6ca053
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-10-31 20:09:27 +0100

    add item to breadcrumbs via breadcrumb service on page component

--- src/app/pages/page/page.component.ts
+++ src/app/pages/page/page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { BreadcrumbService } from "../../../../projects/breadcrumb/src/lib/breadcrumb.service";
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent { }
+export class PageComponent implements OnInit {
+
+  constructor(private breadcrumbService: BreadcrumbService) { }
+
+  ngOnInit(): void {
+    this.breadcrumbService.addItem('Page', '/page');
+  }
+
+}
