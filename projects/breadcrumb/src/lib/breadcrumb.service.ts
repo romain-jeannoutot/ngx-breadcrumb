3779485523d40dc51a5b5d1566bd7135d6ff095f
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-10-31 20:06:35 +0100

    implement breadcrumb service

--- projects/breadcrumb/src/lib/breadcrumb.service.ts
+++ projects/breadcrumb/src/lib/breadcrumb.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { Subject } from "rxjs";
+
+import { Breadcrumb } from "./breadcrumb";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BreadcrumbService {
 
-  constructor() { }
+  public _addItem = new Subject<Breadcrumb>();
+
+  constructor(private router: Router) { }
+
+  addItem(label: string, href: string = this.router.url): void {
+    this._addItem.next(new Breadcrumb(label, href));
+  }
+
 }
